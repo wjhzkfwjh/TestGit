67eb3339f2d4169b24bd1cbb71c5f1b4756b4ec2
wjh <wjhao1990@126.com>
2019-04-24 22:12:33 +0800

    firstcommit

---
+++ src/main/java/com/ths/aifinancialplanner/AifinancialplannerApplication.java
@@ -0,0 +1,13 @@
+package com.ths.aifinancialplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AifinancialplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AifinancialplannerApplication.class, args);
+    }
+
+}
